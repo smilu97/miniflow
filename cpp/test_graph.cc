3d609b549074dd76530df96aced93f92819ac760
smilu97 <smilup2244@gmail.com>
2019-05-17 19:24:04 +0900

    add test_graph

---
+++ cpp/test_graph.cc
@@ -0,0 +1,13 @@
+/*
+ * Test for graph.cc, graph.h
+ */
+
+#include "graph.h"
+#include <cstdio>
+using namespace miniflow;
+
+int main(int argc, char** argv)
+{
+    Node<int> node;
+    puts("Test Graph");
+}
