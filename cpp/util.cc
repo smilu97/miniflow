95ae150effb2d7b8e085a24b4f4dc037c0f24979
smilu97 <smilup2244@gmail.com>
2019-05-18 20:42:39 +0900

    Fix util not instantiating type of (min2, max2)

--- cpp/util.cc
+++ cpp/util.cc
@@ -9,3 +9,8 @@ T max2(T a, T b) { return a < b ? b : a; }
 
 template <class T>
 T min2(T a, T b) { return a < b ? a : b; }
+
+template int min2(int a, int b);
+template float min2(float a, float b);
+template int max2(int a, int b);
+template float max2(float a, float b);
